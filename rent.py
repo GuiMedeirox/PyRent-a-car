1c48976dd0e2c3a723fc5f2bb3c9e426a091214e
Guilherme <omaewa9111@gmail.com>
2023-07-09 10:13:51 -0300

    gg2.0

--- rent.py
+++ rent.py
@@ -1,4 +1,4 @@
-import re
+import re, os
 
 class Carro:
     def __init__(self, modelo, ano, placa):
@@ -21,6 +21,12 @@ def obter_carros():
             carros.append(carro)
     return carros
 
+def buscar_por_placa(placa): 
+    for i in obter_carros(): 
+        if(i.placa == placa): 
+            return f"{i.modelo}, {i.placa}"
+    return "Não há veículo cadastrado com esta placa."
+
 def atualizar_carro(placa, novo_modelo, novo_ano):
     carros = obter_carros()
     for carro in carros:
@@ -42,7 +48,7 @@ class Cliente:
     def __init__(self, nome, cpf):
         self.nome = nome
         self.cpf = cpf
-
+#valeu GPT
 def validar_cpf(cpf):
     cpf = re.sub('[^0-9]', '', cpf)
     if len(cpf) != 11:
@@ -88,6 +94,12 @@ def obter_clientes():
             cliente = Cliente(dados[0], dados[1])
             clientes.append(cliente)
     return clientes
+
+def buscar_por_cpf(cpf): 
+    for i in obter_clientes(): 
+        if(i.cpf == cpf): 
+            return f"{i.nome}, {i.cpf}"
+    return "Não há um cliente cadastrado com o CPF informado. "
 
 def atualizar_cliente(cpf, novo_nome):
     clientes = obter_clientes()
@@ -149,7 +161,6 @@ def atualizar_aluguel(cpf, nova_placa):
     for aluguel in alugueis:
         if aluguel.cpf == cpf:
             aluguel.placa = nova_placa
-            print("Vasco")
     with open("alugueis.txt", "w") as arquivo:
        for aluguel in alugueis:
             arquivo.write(f"{aluguel.cpf},{aluguel.placa}\n")
@@ -163,78 +174,88 @@ def deletar_aluguel(cpf):
             arquivo.write(f"{aluguel.cpf},{aluguel.placa}\n")
 
 def main(): 
-    print("\nPyRent-a-car") 
+    print("\n############")
+    print("PyRent-a-car") 
+    print("\n SESSÃO DOS VEÍCULOS")
     print("01 - LISTAR OS CARROS CADASTRADOS")
-    print("02 - CADASTRAR UM CARRO ")
-    print("03 - EDITAR UM CARRO CADASTRADO ")
-    print("04 - DELETAR UM CARRO ")
-    print("05 - LISTAR OS CLIENTES CADASTRADOS")
-    print("06 - CADASTRAR UM CLIENTE")
-    print("07 - EDITAR INFORMAÇÕES DE UM CLIENTE")
-    print("08 - DELETAR UM CLIENTE")
-    print("09 - LISTAR ALUGUEIS")
-    print("10 - ALUGAR UM CARRO")
-    print("11 - EDITAR ALUGUEL DE CARRO")
-    print("12 - DELETAR ALUGUEL")
-    print("13 - SOBRE O PROJETO") 
-    print("14 - SAIR")
+    print("02 - BUSCA POR PLACA")
+    print("03 - CADASTRAR UM CARRO ")
+    print("04 - EDITAR UM CARRO CADASTRADO ")
+    print("05 - DELETAR UM CARRO ")
+    print("\n SESSÃO DOS CLIENTES")
+    print("06 - LISTAR OS CLIENTES CADASTRADOS")
+    print("07 - BUSCA POR CPF")
+    print("08 - CADASTRAR UM CLIENTE")
+    print("09 - EDITAR INFORMAÇÕES DE UM CLIENTE")
+    print("10 - DELETAR UM CLIENTE")
+    print("\n SESSÃO DOS ALUGUEIS")
+    print("11 - LISTAR ALUGUEIS")
+    print("12 - ALUGAR UM CARRO")
+    print("13 - EDITAR ALUGUEL DE CARRO")
+    print("14 - DELETAR ALUGUEL")
+    print("15 - SOBRE O PROJETO") 
+    print("16 - SAIR")
 
 while True: 
   main()
   option = int(input("\n Digita a opção: "))
+  os.system("clear")
 
   if (option == 1): 
     aux = obter_carros()
     for i in aux: 
         print(f"{i.modelo} {i.placa} {i.ano}")
   elif (option == 2):
+    placa = input("Digite a placa do veículo: ")
+    print(buscar_por_placa(placa))
+  elif (option == 3):
     modelo = input("Digita o modelo do carro: ")
     ano = input("Digita o ano do carro: ")
     placa = input("Digita a placa do carro: ")
     criar_carro(modelo,ano, placa)
-  elif (option == 3): 
+  elif (option == 4): 
     placa = input("\n Placa do carro a ser editado: ")
     modelo = input("\n Novo modelo: ")
     ano = input("\n Novo ano: ")
     atualizar_carro(placa,modelo,ano)
-  elif (option == 4): 
+  elif (option == 5): 
     placa = input("\n Placa do carro: ")
     deletar_carro(placa)
-  elif (option == 5): 
+  elif (option == 6): 
     aux = obter_clientes()
     for i in aux: 
         print(f"{i.nome} {i.cpf}")
-  elif (option == 6): 
+  elif (option == 7): 
+    cpf = input("Digita o CPF a ser buscado: ")
+    print(buscar_por_cpf(cpf))
+  elif (option == 8): 
     nome = input("\nNome do cliente: ")
     cpf = input("\nCPF do cliente: ")
     criar_cliente(nome,cpf)
-  elif (option == 7):
+  elif (option == 9):
     cpf = input("\nCPF do cliente a ser editado: ")
     nome = input("\nNovo nome: ")
     atualizar_cliente(cpf,nome)
-  elif (option == 8): 
+  elif (option == 10): 
     cpf = input("\nDigita o CPF do cliente a ser deletado: ")
     deletar_cliente(cpf)
-  elif (option == 9): 
+  elif (option == 11): 
     aux = obter_alugueis()
     for i in aux: 
         print(f"{i.cpf} {i.placa}")
-  elif (option == 10): 
+  elif (option == 12): 
     cpf = input("\n CPF do cliente: ")
     placa = input("\n Placa do carro: ")
     criar_aluguel(cpf,placa)
-  elif (option == 11): 
+  elif (option == 13): 
     cpf = input("\nInforme o CPF do cliente que terá o veículo editado: ")
     placa = input("\n Informe a nova placa: ")
     atualizar_aluguel(cpf,placa)
-  elif (option == 12): 
+  elif (option == 14): 
     cpf = input("\n Informe o CPF que terá o aluguel deletado: ")
     deletar_aluguel(cpf)
-  elif (option == 13): 
+  elif (option == 15): 
         print("Feito por Guilherme de Medeiros Santos")
         print("PyRent-a-car: o melhor amigo da locadora de veículos!") 
-  elif (option == 14): 
+  elif (option == 16): 
     exit()
-
-
-    
